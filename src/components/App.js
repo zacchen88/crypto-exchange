0467ddabd2b3ea206d99f79025a65e6f5891519b
zac <dulce_chen@hotmail.com>
2023-06-18 01:09:28 +1000

    add Nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,68 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import config from "../config.json";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import config from '../config.json';
 
 import {
   loadProvider,
   loadNetwork,
   loadAccount,
   loadTokens,
-  loadExchange,
-} from "../store/interactions";
+  loadExchange
+} from '../store/interactions';
+
+import Navbar from './Navbar'
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
     // Connect Ethers to blockchain
-    const provider = loadProvider(dispatch);
+    const provider = loadProvider(dispatch)
 
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
-    const chainId = await loadNetwork(provider, dispatch);
+    const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch);
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account & balance from Metamask when changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch)
+    })
 
     // Load token smart contracts
-    const DApp = config[chainId].DApp;
-    const mETH = config[chainId].mETH;
-    await loadTokens(provider, [DApp.address, mETH.address], dispatch);
+    const DApp = config[chainId].DApp
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch)
 
     // Load exchange smart contract
-    const exchangeConfig = config[chainId].exchange;
-    await loadExchange(provider, exchangeConfig.address, dispatch);
-  };
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
+  }
 
   useEffect(() => {
-    loadBlockchainData();
-  });
+    loadBlockchainData()
+  })
 
   return (
     <div>
-      {/* Navbar */}
 
-      <main className="exchange grid">
-        <section className="exchange__section--left grid">
+      <Navbar />
+
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+
           {/* Markets */}
 
           {/* Balance */}
 
           {/* Order */}
+
         </section>
-        <section className="exchange__section--right grid">
+        <section className='exchange__section--right grid'>
+
           {/* PriceChart */}
 
           {/* Transactions */}
@@ -57,10 +70,12 @@ function App() {
           {/* Trades */}
 
           {/* OrderBook */}
+
         </section>
       </main>
 
       {/* Alert */}
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./App.css";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 
-import { Provider } from "react-redux";
-import store from "./store/store";
+import { Provider } from 'react-redux'
+import store from './store/store'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
